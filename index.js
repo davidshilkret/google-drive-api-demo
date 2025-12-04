1405c71bf6708c3da705424917789dbb4fa6f515
David Shilkret <72939561+davidshilkret@users.noreply.github.com>
2025-12-04 10:53:16 -0600

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
-// time.
+// time. //
 const TOKEN_PATH = path.join(process.cwd(), 'token.json');
 
 // Generate OAuth credentials in Google Console
